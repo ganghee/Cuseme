8bc3008f743dc49f50cdbbe94176839368065dd1
ganghee <idiosauri@naver.com>
2020-01-25 20:00:46 +0900

    added postStart method with POST

--- app/src/main/java/com/good/mycuseme/data/remote/NetworkService.kt
+++ app/src/main/java/com/good/mycuseme/data/remote/NetworkService.kt
@@ -1,4 +1,15 @@
 package com.good.mycuseme.data.remote
 
+import com.good.mycuseme.data.start.StartResponse
+import io.reactivex.Single
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+import java.util.*
+
 interface NetworkService {
+
+    @FormUrlEncoded
+    @POST("/auth/start")
+    fun postStart(@Field("uuid")uuid: String) : Single<StartResponse>
 }
