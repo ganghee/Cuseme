0cc01f2421a75ef9c21dfacc10355704a23c0cc1
ganghee <idiosauri@naver.com>
2020-01-25 19:58:45 +0900

    connect network for uuid

--- app/src/main/java/com/good/mycuseme/ui/user/UserViewModel.kt
+++ app/src/main/java/com/good/mycuseme/ui/user/UserViewModel.kt
@@ -1,4 +1,26 @@
 package com.good.mycuseme.ui.user
 
-class UserViewModel {
+import android.annotation.SuppressLint
+import android.util.Log
+import com.good.mycuseme.adapter.CardAdapter
+import com.good.mycuseme.base.BaseViewModel
+import com.good.mycuseme.data.start.StartRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+
+class UserViewModel : BaseViewModel() {
+    val startRepository by lazy { StartRepository() }
+    val cardAdapter by lazy { CardAdapter() }
+
+    @SuppressLint("CheckResult")
+    fun getUUID(uuid: String) {
+        startRepository.postStart(uuid)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("UserViewModel", it.message)
+            }, {
+                Log.d("UserViewModel", it.message)
+            })
+    }
+
+
 }
