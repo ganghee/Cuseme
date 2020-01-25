e539c46879c9b662e48e34e1f5d3c307f212d695
ganghee <idiosauri@naver.com>
2020-01-25 19:57:34 +0900

    input uuid network

--- app/src/main/java/com/good/mycuseme/ui/user/UserActivity.kt
+++ app/src/main/java/com/good/mycuseme/ui/user/UserActivity.kt
@@ -1,13 +1,41 @@
 package com.good.mycuseme.ui.user
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.ContentResolver
+import android.content.Context
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import com.good.mycuseme.R
+import com.good.mycuseme.base.BaseActivity
+import com.good.mycuseme.databinding.ActivityUserBinding
+import com.google.android.material.internal.ContextUtils
+import java.security.AccessController.getContext
 
-class UserActivity : AppCompatActivity() {
+class UserActivity : BaseActivity<ActivityUserBinding>(R.layout.activity_user) {
 
+    private val userViewModel by lazy { ViewModelProvider(this).get(UserViewModel::class.java) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user)
+        binding.userViewModel = userViewModel
+
+        @SuppressLint("HardwareIds")
+        val androidId =  Settings.Secure.getString(
+            this.contentResolver,
+            Settings.Secure.ANDROID_ID)
+
+        initViewModel(androidId)
+        getCard(androidId)
+    }
+
+    private fun getCard(androidId: String) {
+        userViewModel.getCard(androidId)
+    }
+
+    private fun initViewModel(androidId: String) {
+        Toast.makeText(this.applicationContext, androidId, Toast.LENGTH_SHORT).show()
     }
 }
