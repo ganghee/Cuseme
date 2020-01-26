7fd3289702eef74a3612dc982fce23b29ddacf3a
ganghee <idiosauri@naver.com>
2020-01-26 10:00:00 +0900

    added code for POST method of cardVisible

--- app/src/main/java/com/good/mycuseme/data/remote/NetworkService.kt
+++ app/src/main/java/com/good/mycuseme/data/remote/NetworkService.kt
@@ -1,15 +1,19 @@
 package com.good.mycuseme.data.remote
 
+import com.good.mycuseme.data.card.CardResponse
 import com.good.mycuseme.data.start.StartResponse
 import io.reactivex.Single
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
-import java.util.*
 
 interface NetworkService {
 
     @FormUrlEncoded
     @POST("/auth/start")
-    fun postStart(@Field("uuid")uuid: String) : Single<StartResponse>
+    fun postStart(@Field("uuid") uuid: String): Single<StartResponse>
+
+    @FormUrlEncoded
+    @POST("/cards/visible")
+    fun getVisibleCard(@Field("uuid") uuid: String): Single<CardResponse>
 }
