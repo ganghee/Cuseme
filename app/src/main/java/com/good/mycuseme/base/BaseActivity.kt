e2373426376cae9bdc5f98d603e01f73b5091941
ganghee <idiosauri@naver.com>
2020-01-26 10:00:00 +0900

    updated BaseActivity

--- app/src/main/java/com/good/mycuseme/base/BaseActivity.kt
+++ app/src/main/java/com/good/mycuseme/base/BaseActivity.kt
@@ -1,4 +1,18 @@
 package com.good.mycuseme.base
 
-class BaseActivity {
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B : ViewDataBinding>(@LayoutRes private val layoutId: Int) :
+    AppCompatActivity() {
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+    }
 }
