4726c97f60aa48b2fcc5972ed8087e3c193dae07
ganghee <idiosauri@naver.com>
2020-01-26 10:00:00 +0900

    added BaseViewModel

--- app/src/main/java/com/good/mycuseme/base/BaseViewModel.kt
+++ app/src/main/java/com/good/mycuseme/base/BaseViewModel.kt
@@ -1,4 +1,15 @@
 package com.good.mycuseme.base
 
-class BaseViewModel {
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseViewModel : ViewModel() {
+    protected val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        if (!compositeDisposable.isDisposed) {
+            compositeDisposable.dispose()
+        }
+        super.onCleared()
+    }
 }
