91444c7804d68865749657ec24429692b5c1db31
ganghee <idiosauri@naver.com>
2020-01-27 09:12:12 +0900

    updated code

--- app/src/main/java/com/good/mycuseme/data/start/StartRepository.kt
+++ app/src/main/java/com/good/mycuseme/data/start/StartRepository.kt
@@ -2,7 +2,6 @@ package com.good.mycuseme.data.start
 
 import com.good.mycuseme.data.remote.CardRemoteDataSource
 import io.reactivex.Single
-import java.util.*
 
 class StartRepository {
     fun postStart(uuid: String): Single<StartResponse> =
