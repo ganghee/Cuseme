be70b3ef4409e03f7e63072031750e958c34acea
ganghee <idiosauri@naver.com>
2020-01-28 02:39:44 +0900

    added code login xml

--- app/src/main/java/com/good/mycuseme/ui/manage/ManageCardActivity.kt
+++ app/src/main/java/com/good/mycuseme/ui/manage/ManageCardActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.good.mycuseme.R
 
-class ManageActivity : AppCompatActivity() {
+class ManageCardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
