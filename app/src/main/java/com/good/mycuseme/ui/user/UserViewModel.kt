c099ed4e72457e6e7ed528609dd7fc988a421b83
ganghee <idiosauri@naver.com>
2020-01-27 09:17:42 +0900

    added code for sorted cardList and getCard

--- app/src/main/java/com/good/mycuseme/ui/user/UserViewModel.kt
+++ app/src/main/java/com/good/mycuseme/ui/user/UserViewModel.kt
@@ -2,25 +2,57 @@ package com.good.mycuseme.ui.user
 
 import android.annotation.SuppressLint
 import android.util.Log
-import com.good.mycuseme.adapter.CardAdapter
+import androidx.lifecycle.MutableLiveData
 import com.good.mycuseme.base.BaseViewModel
+import com.good.mycuseme.data.card.CardData
+import com.good.mycuseme.data.card.CardRepository
 import com.good.mycuseme.data.start.StartRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
 
 class UserViewModel : BaseViewModel() {
     val startRepository by lazy { StartRepository() }
-    val cardAdapter by lazy { CardAdapter() }
+    val cardRepository by lazy { CardRepository() }
+    val cardList = MutableLiveData<List<CardData>>()
+    val sortList = listOf("basic", "ascent", "count")
+    var index = 0
 
     @SuppressLint("CheckResult")
     fun getUUID(uuid: String) {
         startRepository.postStart(uuid)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                Log.d("UserViewModel", it.message)
+                Log.d("UserViewModel getUUID", it.message)
             }, {
-                Log.d("UserViewModel", it.message)
+                Log.d("UserViewModel getUUID", it.message)
             })
     }
 
+    @SuppressLint("CheckResult")
+    fun getCard(uuid: String) {
+        cardRepository.getVisibleCard(uuid)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("UserViewModel getCard", it.message)
+                sort(it.data)
+            }, {
+                Log.d("UserViewModel getCard", it.message)
+            })
+    }
 
+    fun sort(sortedList: List<CardData>?) {
+        when (sortList[index]) {
+            "basic" -> {
+                cardList.value = sortedList?.sortedBy { it.sequence }
+                index = 1
+            }
+            "ascent" -> {
+                cardList.value = sortedList?.sortedBy { it.title }
+                index = 2
+            }
+            "count" -> {
+                cardList.value = sortedList?.sortedBy { it.count }
+                index = 0
+            }
+        }
+    }
 }
