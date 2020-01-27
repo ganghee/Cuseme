840efe3d7186e107da13cf48ce03af7e10747d56
ganghee <idiosauri@naver.com>
2020-01-28 02:31:36 +0900

    saved user data in local

---
+++ app/src/main/java/com/good/mycuseme/data/local/SharedPreferenceController.kt
@@ -0,0 +1,42 @@
+package com.good.mycuseme.data.local
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.util.Log
+
+object SharedPreferenceController {
+    private const val ACCOUNT = "my account"
+
+    fun setUserInfo(ctx: Context, userData: UserData) {
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = preference.edit()
+        editor.putString("userUUID", userData.uuid)
+        editor.putString("userToken", userData.token)
+        editor.putString("userPhoneNumber", userData.phoneNumber)
+        editor.apply()
+        Log.d("userData", userData.uuid)
+    }
+
+    fun getUserUUID(ctx: Context): String? {
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
+        return preference.getString("userUUID", "")
+    }
+
+    fun getUserToken(ctx: Context): String? {
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
+        return preference.getString("userToken", "")
+    }
+
+    fun getUserPhoneNumber(ctx: Context): String? {
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
+        return preference.getString("userPhoneNumber", "")
+    }
+
+    fun clearUserToken(ctx: Context) {
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = preference.edit()
+        editor.clear()
+        editor.apply()
+    }
+
+}
