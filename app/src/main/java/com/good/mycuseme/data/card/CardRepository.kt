4ed6074b093e6e45fe27cf453bd81469a7af65ac
ganghee <idiosauri@naver.com>
2020-01-31 03:00:00 +0900

    added postDownloadCard method

--- app/src/main/java/com/good/mycuseme/data/card/CardRepository.kt
+++ app/src/main/java/com/good/mycuseme/data/card/CardRepository.kt
@@ -6,4 +6,8 @@ import io.reactivex.Single
 class CardRepository {
     fun getVisibleCard(uuid: String): Single<CardResponse> =
         CardRemoteDataSource.getVisibleCard(uuid)
+
+    fun postDownloadCard(token: String, serialNum: String): Single<DownloadResponse> =
+        CardRemoteDataSource.postDownload(token, serialNum)
+
 }
