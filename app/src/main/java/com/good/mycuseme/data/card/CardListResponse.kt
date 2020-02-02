58e8cd50121015c064280b4deaefbd3476f29301
ganghee <idiosauri@naver.com>
2020-02-02 10:00:00 +0900

    update list response

--- app/src/main/java/com/good/mycuseme/data/card/CardListResponse.kt
+++ app/src/main/java/com/good/mycuseme/data/card/CardListResponse.kt
@@ -3,7 +3,7 @@ package com.good.mycuseme.data.card
 
 import com.google.gson.annotations.SerializedName
 
-data class CardResponse(
+data class CardListResponse(
     @SerializedName("data")
     val `data`: List<CardData>,
     @SerializedName("message")
