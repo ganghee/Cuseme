e457d0fb722648a8fd42cee9170df9bfd4d99098
ganghee <idiosauri@naver.com>
2020-02-02 10:00:00 +0900

    added putUpdateCard method

--- app/src/main/java/com/good/mycuseme/data/card/CardRepository.kt
+++ app/src/main/java/com/good/mycuseme/data/card/CardRepository.kt
@@ -2,12 +2,38 @@ package com.good.mycuseme.data.card
 
 import com.good.mycuseme.data.remote.CardRemoteDataSource
 import io.reactivex.Single
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 class CardRepository {
-    fun getVisibleCard(uuid: String): Single<CardResponse> =
+    fun getVisibleCard(uuid: String): Single<CardListResponse> =
         CardRemoteDataSource.getVisibleCard(uuid)
 
-    fun postDownloadCard(token: String, serialNum: String): Single<DownloadResponse> =
+    fun postDownloadCard(token: String, serialNum: String): Single<CardResponse> =
         CardRemoteDataSource.postDownload(token, serialNum)
+
+    fun postCreateCard(
+        token: String,
+        image: MultipartBody.Part,
+        record: MultipartBody.Part?,
+        title: RequestBody,
+        content: RequestBody,
+        visible: RequestBody
+    ): Single<CardResponse> =
+        CardRemoteDataSource.postCreateCard(token, image, record, title, content, visible)
+
+    fun putUpdateCard(
+        token: String,
+        cardIdx: Int,
+        image: MultipartBody.Part,
+        record: MultipartBody.Part?,
+        title: RequestBody,
+        content: RequestBody,
+        visible: RequestBody
+    ): Single<CardResponse> =
+        CardRemoteDataSource.putUpdateCard(token, cardIdx, image, record, title, content, visible)
+
+    fun getCard(token: String, cardIdx: Int): Single<CardResponse> =
+        CardRemoteDataSource.getCard(token, cardIdx)
 
 }
