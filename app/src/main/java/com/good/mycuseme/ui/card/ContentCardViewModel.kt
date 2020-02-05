b7f9a97a61e96acbbe7134da7d3f659968f2a7bf
ganghee <idiosauri@naver.com>
2020-02-06 04:21:23 +0900

    added detail card information for viewmodel

---
+++ app/src/main/java/com/good/mycuseme/ui/card/ContentCardViewModel.kt
@@ -0,0 +1,110 @@
+package com.good.mycuseme.ui.card
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.media.MediaPlayer
+import android.speech.tts.TextToSpeech
+import android.speech.tts.UtteranceProgressListener
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import com.good.mycuseme.base.BaseViewModel
+import com.good.mycuseme.data.card.CardRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import retrofit2.HttpException
+import java.io.IOException
+import java.util.*
+
+class ContentCardViewModel : BaseViewModel() {
+    val title = MutableLiveData<String>()
+    val content = MutableLiveData<String>()
+    val image = MutableLiveData<String>()
+    val serialNumber = MutableLiveData<String>()
+    var recordFileName: String? = null
+    var recordFlag = 0
+    private val player: MediaPlayer by lazy { MediaPlayer() }
+    val isVisibility = MutableLiveData<Boolean>().apply {
+        value = false
+    }
+    private lateinit var textToSpeech: TextToSpeech
+
+
+    private val repository by lazy { CardRepository() }
+
+    @SuppressLint("CheckResult")
+    fun getCard(token: String, cardIdx: Int) {
+        repository.getCard(token, cardIdx)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                title.value = it.cardData?.title
+                content.value = it.cardData?.content
+                recordFileName = it.cardData?.record
+                serialNumber.value = "일련번호 | " + it.cardData?.serialNum
+                image.value = it.cardData?.image
+                isVisibility.value = it.cardData?.visible
+            }, { error ->
+                error as HttpException
+                error.message()
+            })
+    }
+
+    fun setTextToSpeech(context: Context) {
+        textToSpeech = TextToSpeech(context,
+            TextToSpeech.OnInitListener { status ->
+                if (status == TextToSpeech.SUCCESS) {
+                    textToSpeech.setLanguage(Locale.KOREA).let {
+                        if (it == TextToSpeech.LANG_MISSING_DATA
+                            || it == TextToSpeech.LANG_NOT_SUPPORTED
+                        ) {
+                            Toast.makeText(context, "지금 지원되지 않습니다.", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            })
+    }
+
+    fun startPlay() {
+        if (recordFlag == 0) {
+            if (recordFileName == null) {
+                textToSpeech.speak(
+                    content.value,
+                    TextToSpeech.QUEUE_FLUSH,
+                    null,
+                    null
+                )
+                val speechListener = object : UtteranceProgressListener() {
+                    override fun onDone(utteranceId: String?) {
+                        recordFlag = 0
+                    }
+
+                    override fun onError(utteranceId: String?) {
+                        Log.d("recordError  ", utteranceId.toString())
+                    }
+
+                    override fun onStart(utteranceId: String?) {
+                        recordFlag = -1
+                    }
+                }
+                textToSpeech.setOnUtteranceProgressListener(speechListener)
+            } else {
+                player.apply {
+                    try {
+                        reset() //매우 중요 초기화 되지 않은 처음 생태로 되돌리며 이후 재 초기화 하여 다시 사용 할 수 있다. release는 객체를 완전히 파괴하여 더 이상 사용할 수 없는 상태이다.
+                        setDataSource(recordFileName)
+                        prepareAsync()   //비동기
+                        setOnPreparedListener {
+                            it.start()
+                        }
+                        recordFlag = -1
+                    } catch (e: IOException) {
+                        Log.d("playerError", e.message.toString())
+                    }
+                }
+                player.setOnCompletionListener {
+                    Log.d("playerError2", "SDSD")
+                    recordFlag = 0
+                }
+            }
+        }
+    }
+}
