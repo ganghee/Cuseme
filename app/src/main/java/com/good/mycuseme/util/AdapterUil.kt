0fa8eabdd66676bd9450699e828e6d6dd5ac5707
ganghee <idiosauri@naver.com>
2020-02-06 04:42:13 +0900

    added code for permision

--- app/src/main/java/com/good/mycuseme/util/AdapterUil.kt
+++ app/src/main/java/com/good/mycuseme/util/AdapterUil.kt
@@ -1,16 +1,66 @@
 package com.good.mycuseme.util
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.provider.Settings
+import android.util.Log
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.good.mycuseme.base.BaseRecyclerViewAdapter
 
 @BindingAdapter("setImage")
 fun setImageUrl(view: ImageView, profile: String?) {
     Glide.with(view.context)
         .load(profile)
+        .listener(createLoggerListener("original"))
+        .override(500, 500)
         .into(view)
+}
+
+private fun createLoggerListener(name: String): RequestListener<Drawable> {
+    return object : RequestListener<Drawable> {
+        override fun onLoadFailed(
+            e: GlideException?,
+            model: Any?,
+            target: com.bumptech.glide.request.target.Target<Drawable>?,
+            isFirstResource: Boolean
+        ): Boolean {
+            return false
+        }
+
+        override fun onResourceReady(
+            resource: Drawable?,
+            model: Any?,
+            target: Target<Drawable>?,
+            dataSource: com.bumptech.glide.load.DataSource?,
+            isFirstResource: Boolean
+        ): Boolean {
+            if (resource is BitmapDrawable) {
+                val bitmap = resource.bitmap
+                Log.d(
+                    "GlideApp",
+                    String.format(
+                        "Ready %s bitmap %,d bytes, size: %d x %d",
+                        name,
+                        bitmap.byteCount,
+                        bitmap.width,
+                        bitmap.height
+                    )
+                )
+            }
+            return false
+        }
+    }
 }
 
 @BindingAdapter(value = ["replaceAll"])
@@ -21,3 +71,18 @@ fun RecyclerView.replaceAll(list: List<Any>?) {
         notifyDataSetChanged()
     }
 }
+
+fun Context.startSettingActivity() {
+    startActivity(Intent().apply {
+        action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+        data = Uri.fromParts("package", packageName, null)
+    })
+}
+
+fun Context.isPermissionNotGranted(permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        this,
+        permission
+    ) != PackageManager.PERMISSION_GRANTED
+}
+
