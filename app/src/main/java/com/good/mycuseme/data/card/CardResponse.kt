bebd053db632b7f628b419e50f4e862d5bafe1b2
ganghee <idiosauri@naver.com>
2020-02-06 04:30:36 +0900

    update CardData for nullable

--- app/src/main/java/com/good/mycuseme/data/card/CardResponse.kt
+++ app/src/main/java/com/good/mycuseme/data/card/CardResponse.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 
 data class CardResponse(
     @SerializedName("data")
-    val cardData: CardData,
+    val cardData: CardData?,
     @SerializedName("message")
     val message: String,
     @SerializedName("status")
