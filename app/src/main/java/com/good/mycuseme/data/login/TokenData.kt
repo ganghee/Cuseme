2c9959efed62ea7f2b448bd41ee9e2283e73c716
ganghee <idiosauri@naver.com>
2020-02-06 19:59:47 +0900

    added code for changing password

--- app/src/main/java/com/good/mycuseme/data/login/TokenData.kt
+++
@@ -1,9 +0,0 @@
-package com.good.mycuseme.data.login
-
-
-import com.google.gson.annotations.SerializedName
-
-data class TokenData(
-    @SerializedName("token")
-    val token: String
-)
---
+++ app/src/main/java/com/good/mycuseme/ui/setting/PasswordActivity.kt
@@ -0,0 +1,72 @@
+package com.good.mycuseme.ui.setting
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.good.mycuseme.R
+import com.good.mycuseme.base.BaseActivity
+import com.good.mycuseme.data.local.SharedPreferenceController
+import com.good.mycuseme.databinding.ActivityPasswordBinding
+import kotlinx.android.synthetic.main.activity_password.*
+import kotlinx.android.synthetic.main.toolbar_back.*
+
+class PasswordActivity : BaseActivity<ActivityPasswordBinding>(R.layout.activity_password) {
+
+    private val passwordViewModel by lazy { ViewModelProvider(this).get(PasswordViewModel::class.java) }
+    val token by lazy { SharedPreferenceController.getUserToken(this) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.passwordViewModel = passwordViewModel
+
+        buttonClickable()
+        changePassword()
+        backButton()
+    }
+
+    private fun backButton() {
+        iv_back.setOnClickListener { finish() }
+    }
+
+    private fun changePassword() {
+        passwordViewModel.apply {
+            btn_password.setOnClickListener {
+                changePassword(
+                    token!!,
+                    passwordViewModel.beforePassword.value!!,
+                    passwordViewModel.newPassword.value!!
+                )
+            }
+            successChange.observe(this@PasswordActivity, Observer {
+                if (it == 1) Toast.makeText(
+                    this@PasswordActivity,
+                    "비밀번호가 변경되었습니다.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                else if (it == -1) Toast.makeText(
+                    this@PasswordActivity,
+                    "비밀번호가 변경되지 않았습니다.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            })
+        }
+    }
+
+    private fun buttonClickable() {
+        passwordViewModel.apply {
+            isClickable.observe(this@PasswordActivity, Observer {
+                btn_password.isSelected = it
+            })
+            beforePassword.observe(this@PasswordActivity, Observer {
+                confirmNewPassword()
+            })
+            newPassword.observe(this@PasswordActivity, Observer {
+                confirmNewPassword()
+            })
+            newPasswordConfirm.observe(this@PasswordActivity, Observer {
+                confirmNewPassword()
+            })
+        }
+    }
+}
