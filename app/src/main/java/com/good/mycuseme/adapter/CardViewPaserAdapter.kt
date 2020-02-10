4a7cc768cea2352794aecb509174d450512ec46b
ganghee <idiosauri@naver.com>
2020-02-11 01:42:52 +0900

    updated fragment for edit card class

--- app/src/main/java/com/good/mycuseme/adapter/CardViewPaserAdapter.kt
+++ app/src/main/java/com/good/mycuseme/adapter/CardViewPaserAdapter.kt
@@ -1,22 +1,28 @@
 package com.good.mycuseme.adapter
 
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.good.mycuseme.ui.manage.SearchListFragment
+import com.good.mycuseme.ui.manage.PreviewArrayFragment
+import com.good.mycuseme.ui.manage.PreviewRearrayFragment
 
 
 class CategoryViewPagerAdapter(fm: FragmentManager) :
     FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
-    private val cardTitleList =
-        listOf("보이는 순", "이름 순", "조회 순")
+    val cardTitleList =
+        listOf("재배열", "이름 순", "조회 순")
 
-    private val cardList =
+    val cardList =
         listOf("visibility", "title", "count")
 
-    override fun getItem(position: Int): Fragment =
-        SearchListFragment.newInstance(cardList[position])
+    override fun getItem(position: Int): Fragment {
+        Log.d("position", position.toString())
+        return if(position == 0) PreviewRearrayFragment()
+        else PreviewArrayFragment.newInstance(cardList[position])
+
+    }
 
     override fun getCount(): Int = cardList.size
 
