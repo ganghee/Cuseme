f9a3795b21c55b17c9489a91afd6cd4467cdc528
ganghee <idiosauri@naver.com>
2020-02-11 03:35:03 +0900

    added token data class

--- app/src/main/java/com/good/mycuseme/data/user/UserResponse.kt
+++ app/src/main/java/com/good/mycuseme/data/user/UserResponse.kt
@@ -1,15 +1,19 @@
-package com.good.mycuseme.data.login
-
+package com.good.mycuseme.data.user
 
 import com.google.gson.annotations.SerializedName
 
-data class LoginResponse(
+data class UserResponse(
     @SerializedName("data")
-    val tokenData: TokenData,
+    val tokenData: TokenData?,
     @SerializedName("message")
     val message: String,
     @SerializedName("status")
     val status: Int,
     @SerializedName("success")
     val success: Boolean
+)
+
+data class TokenData(
+    @SerializedName("token")
+    val token: String
 )
