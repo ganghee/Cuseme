5643209d54c4fb15f9491eeb5f87b82b17622a4b
ganghee <idiosauri@naver.com>
2020-02-11 03:34:19 +0900

    implement changePhoneNumber and changePassword code

--- app/src/main/java/com/good/mycuseme/data/user/UserRepository.kt
+++ app/src/main/java/com/good/mycuseme/data/user/UserRepository.kt
@@ -1,9 +1,15 @@
-package com.good.mycuseme.data.login
+package com.good.mycuseme.data.user
 
 import com.good.mycuseme.data.remote.CardRemoteDataSource
 import io.reactivex.Single
 
-class LoginRepository {
-    fun postLogin(uuid: String, password: String): Single<LoginResponse> =
+class UserRepository {
+    fun postLogin(uuid: String, password: String): Single<UserResponse> =
         CardRemoteDataSource.postLogin(uuid, password)
+
+    fun changePhoneNumber(token: String, phoneNumber: String) =
+        CardRemoteDataSource.changePhoneNumber(token, phoneNumber)
+
+    fun changePassword(token: String, password: String, newPassword: String) =
+        CardRemoteDataSource.changePassword(token, password, newPassword)
 }
