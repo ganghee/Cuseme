5385209699fd3fc29c5ac52f3dd83dadc3a79a12
ganghee <idiosauri@naver.com>
2020-02-11 03:37:31 +0900

    seperate user infomation

--- app/src/main/java/com/good/mycuseme/data/local/SharedPreferenceController.kt
+++ app/src/main/java/com/good/mycuseme/data/local/SharedPreferenceController.kt
@@ -6,12 +6,24 @@ import android.content.SharedPreferences
 object SharedPreferenceController {
     private const val ACCOUNT = "my account"
 
-    fun setUserInfo(ctx: Context, userData: UserData) {
+    fun setUserUUID(ctx: Context, uuid: String) {
         val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
         val editor: SharedPreferences.Editor = preference.edit()
-        editor.putString("userUUID", userData.uuid)
-        editor.putString("userToken", userData.token)
-        editor.putString("userPhoneNumber", userData.phoneNumber)
+        editor.putString("userUUID", uuid)
+        editor.apply()
+    }
+
+    fun setUserToken(ctx: Context, token: String) {
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = preference.edit()
+        editor.putString("userToken", token)
+        editor.apply()
+    }
+
+    fun setUserPhoneNumber(ctx: Context, phone: String) {
+        val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = preference.edit()
+        editor.putString("userPhoneNumber", phone)
         editor.apply()
     }
 
@@ -22,12 +34,12 @@ object SharedPreferenceController {
 
     fun getUserToken(ctx: Context): String? {
         val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
-        return preference.getString("userToken", "")
+        return preference.getString("userToken", "default token")
     }
 
     fun getUserPhoneNumber(ctx: Context): String? {
         val preference: SharedPreferences = ctx.getSharedPreferences(ACCOUNT, Context.MODE_PRIVATE)
-        return preference.getString("userPhoneNumber", "")
+        return preference.getString("userPhoneNumber", "010-")
     }
 
     fun clearUserToken(ctx: Context) {
