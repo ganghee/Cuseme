f0b40e42681ed1961e8989cdb22cd31cc02d9744
ganghee <idiosauri@naver.com>
2020-02-11 03:39:29 +0900

    implement back click event

--- app/src/main/java/com/good/mycuseme/ui/card/DownloadActivity.kt
+++ app/src/main/java/com/good/mycuseme/ui/card/DownloadActivity.kt
@@ -2,15 +2,16 @@ package com.good.mycuseme.ui.card
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import androidx.core.view.isVisible
+import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.good.mycuseme.R
 import com.good.mycuseme.base.BaseActivity
 import com.good.mycuseme.data.local.SharedPreferenceController
 import com.good.mycuseme.databinding.ActivityDownloadBinding
+import com.good.mycuseme.ui.manage.ManageCardActivity
 import kotlinx.android.synthetic.main.activity_download.*
+import kotlinx.android.synthetic.main.toolbar_card.*
 
 class DownloadActivity : BaseActivity<ActivityDownloadBinding>(R.layout.activity_download) {
 
@@ -24,6 +25,11 @@ class DownloadActivity : BaseActivity<ActivityDownloadBinding>(R.layout.activity
         initButton()
         downloadCard(token!!)
         startCreateActivity()
+        backButton()
+    }
+
+    private fun backButton() {
+        iv_back.setOnClickListener { finish() }
     }
 
     private fun startCreateActivity() {
@@ -36,7 +42,6 @@ class DownloadActivity : BaseActivity<ActivityDownloadBinding>(R.layout.activity
 
     private fun downloadCard(token: String) {
         btn_download.setOnClickListener {
-            Log.d("tag", token + "   " + downloadViewModel.serialNumber.value!!)
             downloadViewModel.downloadCard(token, downloadViewModel.serialNumber.value!!)
         }
     }
@@ -48,8 +53,18 @@ class DownloadActivity : BaseActivity<ActivityDownloadBinding>(R.layout.activity
                 btn_download.isSelected = isClickable.value!!
             })
             wrongDownload.observe(this@DownloadActivity, Observer {
-                tv_download_wrong_text.isVisible = downloadSuccess.value!!
+                if (it) tv_download_wrong_text.visibility = View.VISIBLE
+            })
+            downloadSuccess.observe(this@DownloadActivity, Observer {
+                if (it) finish()
             })
         }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, ManageCardActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
