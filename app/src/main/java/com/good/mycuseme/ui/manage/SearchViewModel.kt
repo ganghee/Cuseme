3d5b3905c9548ec880a459adab5bf24b9afc296c
ganghee <idiosauri@naver.com>
2020-02-19 18:29:52 +0900

    implements getting cards

--- app/src/main/java/com/good/mycuseme/ui/manage/SearchViewModel.kt
+++ app/src/main/java/com/good/mycuseme/ui/manage/SearchViewModel.kt
@@ -1,5 +1,24 @@
 package com.good.mycuseme.ui.manage
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import com.good.mycuseme.base.BaseViewModel
+import com.good.mycuseme.data.card.CardData
+import com.good.mycuseme.data.card.CardRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
 
-class SearchViewModel : BaseViewModel()
+class SearchViewModel : BaseViewModel() {
+
+    private val cardRepository by lazy { CardRepository() }
+    var cardList = MutableLiveData<List<CardData>>()
+
+    fun getAllCard(token: String) =
+        cardRepository.getAllCard(token)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                Log.d("SearchViewModel getCard", it.message)
+                cardList.value = it.data
+            }, {
+                Log.d("SearchViewModel getCard", it.message!!)
+            })
+}
