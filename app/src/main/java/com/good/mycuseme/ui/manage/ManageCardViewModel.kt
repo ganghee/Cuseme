b2f33e7e2844be4491e13dceccd122e6f2021c00
ganghee <idiosauri@naver.com>
2020-03-01 15:50:13 +0900

    added back button flag

--- app/src/main/java/com/good/mycuseme/ui/manage/ManageCardViewModel.kt
+++ app/src/main/java/com/good/mycuseme/ui/manage/ManageCardViewModel.kt
@@ -1,5 +1,24 @@
 package com.good.mycuseme.ui.manage
 
+import androidx.lifecycle.MutableLiveData
 import com.good.mycuseme.base.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.BehaviorSubject
 
-class ManageCardViewModel : BaseViewModel()
+class ManageCardViewModel : BaseViewModel() {
+
+    val finishFlag = MutableLiveData<Boolean>()
+    private lateinit var backPressedDisposable: Disposable
+    val backButtonSubject: io.reactivex.subjects.Subject<Long> =
+        BehaviorSubject.createDefault(0L)
+
+    fun getFinishFlag() {
+        backPressedDisposable = backButtonSubject
+            .buffer(2, 1)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { t ->
+                finishFlag.value = t[1] - t[0] <= 1500
+            }
+    }
+}
